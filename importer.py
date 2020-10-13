4075db0f21db82a3bfed223a60aa1115efb82667
Ezra Brooks <ezra.m.brooks@gmail.com>
2020-10-12 20:55:49 -0400

    Refactor import for better perf and error handling

--- importer.py
+++ importer.py
@@ -3,9 +3,47 @@ import os
 from time import strptime
 
 import requests
-import tle2czml
 from bs4 import BeautifulSoup
 from pymongo import TEXT, MongoClient
+
+import tle2czml
+
+
+def parse_satcat_entry(satcat_entry: str):
+    launch_date = None
+    decay_date = None
+    try:
+        launch_date = strptime(satcat_entry[56:66].strip(), r'%Y-%m-%d')
+        decay_date = strptime(satcat_entry[75:85].strip(), r'%Y-%m-%d')
+    finally:
+        return {
+            'designator': satcat_entry[0:12].strip(),
+            'noradCatalogNumber': satcat_entry[13:18].strip(),
+            'hasMultipleNames': satcat_entry[19:20] == 'M',
+            'isPayload': satcat_entry[20:21] == '*',
+            'operationalStatusCode': satcat_entry[21:22],
+            'names': satcat_entry[23:47].strip(),
+            'sourceOrOwnership': satcat_entry[49:54].strip(),
+            'launchDate': launch_date,
+            'launchSite': satcat_entry[68:73].strip(),
+            'decayDate': decay_date,
+            'orbitalPeriod': satcat_entry[87:94].strip(),
+            'inclination': satcat_entry[96:101].strip(),
+            'apogeeAltitude': satcat_entry[103:109].strip(),
+            'perigeeAltitude': satcat_entry[111:117].strip(),
+            'radarCrossSection': satcat_entry[119:127].strip(),
+            'orbitalStatusCode': satcat_entry[129:132].strip()
+        }
+
+def get_tle(norad_catalog_number: str):
+    tle_text = requests.get(
+            'https://celestrak.com/satcat/tle.php?CATNR=' + satellite['noradCatalogNumber']).text
+    soup = BeautifulSoup(tle_text, features='html.parser')
+    tle_element = soup.select_one('pre')
+    if tle_element is not None:
+        return tle_element.string.strip()
+    else:
+        return None
 
 mongo_uri = "mongodb://localhost:27017/orbits"
 try:
@@ -21,50 +59,18 @@ satellite_collection = db['satellites']
 # Retrieve CelesTrak's entire SATCAT (satellite catalog)
 satcat_text: str = requests.get(
     "https://celestrak.com/pub/satcat.txt").text
-# print(satcat_text)
-for satcat_entry in satcat_text.splitlines():
-    designator = satcat_entry[0:12].strip()
-    norad_catalog_number = satcat_entry[13:18].strip()
-    has_multiple_names = satcat_entry[19:20] == 'M'
-    is_payload = satcat_entry[20:21] == '*'
-    operational_status_code = satcat_entry[21:22]
-    names = satcat_entry[23:47].strip()
-    source_or_ownership = satcat_entry[49:54].strip()
-    launch_date = strptime(satcat_entry[56:66].strip(), r'%Y-%m-%d')
-    launch_site = satcat_entry[68:73].strip()
-    try:
-        decay_date = strptime(satcat_entry[75:85].strip(), r'%Y-%m-%d')
-    except ValueError as e:
-        decay_date = None
-    orbital_period = satcat_entry[87:94].strip()
-    inclination = satcat_entry[96:101].strip()
-    apogee_altitude = satcat_entry[103:109].strip()
-    perigee_altitude = satcat_entry[111:117].strip()
-    radar_cross_section = satcat_entry[119:127].strip()
-    orbital_status_code = satcat_entry[129:132].strip()
-    satellite_collection.replace_one({'designator': designator}, {
-        'designator': designator,
-        'noradCatalogNumber': norad_catalog_number,
-        'hasMultipleNames': has_multiple_names,
-        'isPayload': is_payload,
-        'operationalStatusCode': operational_status_code,
-        'names': names,
-        'sourceOrOwnership': source_or_ownership,
-        'launchDate': launch_date,
-        'launchSite': launch_site,
-        'orbitalPeriod': orbital_period,
-        'inclination': inclination,
-        'apogeeAltitude': apogee_altitude,
-        'perigeeAltitude': perigee_altitude,
-        'radarCrossSection': radar_cross_section,
-        'orbitalStatusCode': orbital_status_code
-    }, upsert=True)
-    # If this SATCAT entry is a mission payload and is operational or partially operational, go get its TLE.
-    if is_payload and (operational_status_code == '+' or operational_status_code == 'P' or operational_status_code == 'B' or operational_status_code == 'S' or operational_status_code == 'X'):
-        tle = requests.get(
-            'https://celestrak.com/satcat/tle.php?CATNR=' + norad_catalog_number).text
-        soup = BeautifulSoup(tle, features='html.parser')
-        tle = soup.select_one('pre').string.strip()
+
+satellites = [parse_satcat_entry(satellite) for satellite in satcat_text.splitlines()]
+for satellite in satellites:
+    satellite_collection.replace_one({'designator': satellite['designator']}, satellite, upsert=True)
+
+# If this SATCAT entry is a mission payload and is operational or partially operational, go get its TLE.
+operational_payloads = [satellite for satellite in satellites if satellite['isPayload'] and satellite['operationalStatusCode'] in ['+', 'P', 'B', 'S', 'X']]
+
+tles = [get_tle(payload['noradCatalogNumber']) for payload in operational_payloads]
+
+for tle in tles:
+    if tle is not None:
         czml = json.loads(tle2czml.tles_to_czml(tle, silent=True))
         for entry in czml:
             czml_collection.replace_one(
