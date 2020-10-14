c2a6978602e378faa5a1b6e18a0d47ee6b750a8e
Ezra Brooks <ezra.m.brooks@gmail.com>
2020-10-13 23:50:55 -0400

    More performance improvements, add progress bars

--- importer.py
+++ importer.py
@@ -1,10 +1,11 @@
-import json
 import os
 from time import strptime
 
 import requests
 from bs4 import BeautifulSoup
+from bson import json_util
 from pymongo import TEXT, MongoClient
+from tqdm import tqdm
 
 import tle2czml
 
@@ -37,7 +38,7 @@ def parse_satcat_entry(satcat_entry: str):
 
 def get_tle(norad_catalog_number: str):
     tle_text = requests.get(
-            'https://celestrak.com/satcat/tle.php?CATNR=' + satellite['noradCatalogNumber']).text
+            'https://celestrak.com/satcat/tle.php?CATNR=' + norad_catalog_number).text
     soup = BeautifulSoup(tle_text, features='html.parser')
     tle_element = soup.select_one('pre')
     if tle_element is not None:
@@ -55,25 +56,28 @@ mongo_client = MongoClient(mongo_uri)
 db = mongo_client.get_database()
 czml_collection = db['czml']
 satellite_collection = db['satellites']
+czml_collection.drop()
+satellite_collection.drop()
 
 # Retrieve CelesTrak's entire SATCAT (satellite catalog)
 satcat_text: str = requests.get(
     "https://celestrak.com/pub/satcat.txt").text
 
-satellites = [parse_satcat_entry(satellite) for satellite in satcat_text.splitlines()]
-for satellite in satellites:
-    satellite_collection.replace_one({'designator': satellite['designator']}, satellite, upsert=True)
+print("Parsing SATCAT entries...")
+satellites = [parse_satcat_entry(satellite) for satellite in tqdm(satcat_text.splitlines())]
+print("Inserting SATCAT entries into database...")
+satellite_collection.insert_many(satellites)
 
 # If this SATCAT entry is a mission payload and is operational or partially operational, go get its TLE.
 operational_payloads = [satellite for satellite in satellites if satellite['isPayload'] and satellite['operationalStatusCode'] in ['+', 'P', 'B', 'S', 'X']]
 
-tles = [get_tle(payload['noradCatalogNumber']) for payload in operational_payloads]
+print("Retrieving TLEs...")
+tles = [get_tle(payload['noradCatalogNumber']) for payload in tqdm(operational_payloads)]
 
-for tle in tles:
-    if tle is not None:
-        czml = json.loads(tle2czml.tles_to_czml(tle, silent=True))
-        for entry in czml:
-            czml_collection.replace_one(
-                {'id': entry['id']}, entry, upsert=True)
+print("Converting TLEs to CZML...")
+czml_documents = [json_util.loads(tle2czml.tles_to_czml(tle, silent=True)) for tle in tqdm(tles) if tle is not None]
+
+for czml_document in czml_documents:
+    czml_collection.insert_many(czml_document)
 
 czml_collection.create_index([('id', TEXT)])
