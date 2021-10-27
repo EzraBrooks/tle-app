c37e89587c28208219dcd55d85189d73ac8ce33c
Ezra Brooks <ezra.m.brooks@gmail.com>
2021-10-26 22:14:30 -0400

    Retrieve all TLEs at once

--- importer.py
+++ importer.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import os
 from time import strptime
 
@@ -36,15 +37,11 @@ def parse_satcat_entry(satcat_entry: str):
             'orbitalStatusCode': satcat_entry[129:132].strip()
         }
 
-def get_tle(norad_catalog_number: str):
-    tle_text = requests.get(
-            'https://celestrak.com/satcat/tle.php?CATNR=' + norad_catalog_number).text
-    soup = BeautifulSoup(tle_text, features='html.parser')
-    tle_element = soup.select_one('pre')
-    if tle_element is not None:
-        return tle_element.string.strip()
-    else:
-        return None
+
+def get_tles():
+    return requests.get(
+        'https://celestrak.com/NORAD/elements/active.txt').text
+
 
 mongo_uri = "mongodb://localhost:27017/orbits"
 try:
@@ -64,19 +61,19 @@ satcat_text: str = requests.get(
     "https://celestrak.com/pub/satcat.txt").text
 
 print("Parsing SATCAT entries...")
-satellites = [parse_satcat_entry(satellite) for satellite in tqdm(satcat_text.splitlines())]
+satellites = [parse_satcat_entry(satellite)
+              for satellite in tqdm(satcat_text.splitlines())]
 print("Inserting SATCAT entries into database...")
 satellite_collection.insert_many(satellites)
 
-# If this SATCAT entry is a mission payload and is operational or partially operational, go get its TLE.
-operational_payloads = [satellite for satellite in satellites if satellite['isPayload'] and satellite['operationalStatusCode'] in ['+', 'P', 'B', 'S', 'X']]
-
 print("Retrieving TLEs...")
-tles = [get_tle(payload['noradCatalogNumber']) for payload in tqdm(operational_payloads)]
+tles = get_tles()
 
 print("Converting TLEs to CZML...")
-czml_documents = [json_util.loads(tle2czml.tles_to_czml(tle, silent=True)) for tle in tqdm(tles) if tle is not None]
+czml_documents = [json_util.loads(tle2czml.tles_to_czml(
+    tles, silent=True))]
 
+print("Inserting CZML into MongoDB...")
 for czml_document in czml_documents:
     czml_collection.insert_many(czml_document)
 
