8da66716f19e91465a9d64116d7199a8435626fe
Ezra Brooks <ezra.m.brooks@gmail.com>
2021-11-06 10:38:46 -0400

    Initial commit of Next.js port

--- static/index.js
+++
@@ -1,9 +0,0 @@
-// Might as well just leave this in the code since it's going to be blatantly available on the frontend anyway. pls no steal.
-Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI5ZTVkZTc3ZS0yODA4LTQwYmEtODhiMS0wOTgwYWI0YmM4YjkiLCJpZCI6Mzg0OSwic2NvcGVzIjpbImFzciIsImdjIl0sImlhdCI6MTUzOTA2MDMzOX0.U8fGq4kSS6zB9sIHYR8knd5Y07EbhjMiYu3aHucmtok';
-
-window.addEventListener('load', function () {
-    var viewer = new Cesium.Viewer(document.getElementById('cesium-container'));
-    viewer.dataSources.add(Cesium.CzmlDataSource.load('/orbits'));
-    viewer.clock.canAnimate = true;
-    viewer.clock.shouldAnimate = true;
-})
